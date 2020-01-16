2bdd1c84f455cf70e6b46fea391f23416f3246fb
Empiezan <michaelchang90@gmail.com>
2020-01-15 21:52:13 -0600

    Initial Commit

---
+++ OurCharts/OurCharts.h
@@ -0,0 +1,19 @@
+//
+//  OurCharts.h
+//  OurCharts
+//
+//  Created by Michael Chang on 1/15/20.
+//  Copyright © 2020 Michael Chang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OurCharts.
+FOUNDATION_EXPORT double OurChartsVersionNumber;
+
+//! Project version string for OurCharts.
+FOUNDATION_EXPORT const unsigned char OurChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OurCharts/PublicHeader.h>
+
+
